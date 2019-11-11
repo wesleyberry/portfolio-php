55fc8e88fae6dcb78977b0237a75fe7f9378f4b6
Wesley Berry <wesleyberry52@gmail.com>
2019-11-11 15:18:17 -0600

    fixed navbar item spacing

--- views/blog.php
+++ views/blog.php
@@ -48,7 +48,7 @@
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="/">Home</a>
+                    <a class="nav-link" href="/">Portfolio</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="/blogs">Blog <span class="sr-only">(current)</span></a>
